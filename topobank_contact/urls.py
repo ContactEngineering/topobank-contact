c163c43956a038ebdf26200cc166e0fae5dc9ddb
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-01-27 15:32:37 +0100

    MAINT: App name is `topobank_contact`

--- topobank_contact/urls.py
+++ topobank_contact/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from django.contrib.auth.decorators import login_required
 
 
-app_name = "contact"
+app_name = "topobank_contact"
 urlpatterns = [
     # Define extra urls here
 ]
