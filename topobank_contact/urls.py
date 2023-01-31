1d61ee8f73c6f42811ef50d41249157ac14ed896
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-01-31 11:25:11 +0100

    MAINT: Reverted app_name (removed 'topobank_' prefix) and added comment

--- topobank_contact/urls.py
+++ topobank_contact/urls.py
@@ -1,4 +1,7 @@
-app_name = "topobank_contact"
+# App name determines the internal name space, e.g. example can be references
+# as 'contact:example'
+app_name = 'contact'
+
 urlpatterns = [
     # Define extra urls here
 ]
