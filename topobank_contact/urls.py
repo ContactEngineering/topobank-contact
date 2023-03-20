ff86720cda059517592ca44fe8d330bbdb739adf
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-03-20 09:02:17 +0100

    ENH: Contact mechanics card route

--- topobank_contact/urls.py
+++ topobank_contact/urls.py
@@ -1,7 +1,18 @@
 # App name determines the internal name space, e.g. example can be references
 # as 'contact:example'
-app_name = 'contact'
 
+from .functions import APP_NAME, VIZ_CONTACT_MECHANICS
+
+app_name = APP_NAME
 urlpatterns = [
-    # Define extra urls here
+    # POST
+    # * Triggers analyses if not yet running
+    # * Return state of analyses
+    # * Return plot configuration for finished analyses
+    # This is a post request because the request parameters are complex.
+    path(
+        f'card/{VIZ_CONTACT_MECHANICS}',
+        view=login_required(roughness_parameters_card_view),
+        name=f'card-{VIZ_CONTACT_MECHANICS}'
+    ),
 ]
