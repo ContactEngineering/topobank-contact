db0551d6860636f4accac0879ee5af56523dfa75
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-01-18 21:29:38 +0100

    TST: Removed home view

--- topobank_contact/tests/urls.py
+++ topobank_contact/tests/urls.py
@@ -6,10 +6,7 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from ce_ui.views import HomeView
-
 urlpatterns = [
-    path("", HomeView.as_view(), name="home"),
     path(
         "manager/",
         include("topobank.manager.urls", namespace="manager"),
@@ -19,6 +16,6 @@ urlpatterns = [
         include("topobank.analysis.urls", namespace="analysis"),
     ),
     path("accounts/", include("allauth.urls")),
-] + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
